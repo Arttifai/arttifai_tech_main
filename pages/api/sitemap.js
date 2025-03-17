a7c35fb364e34e801470a87f08b23d6d11a0d44e
Arttifai Tech <arttifai@gmail.com>
2025-03-17 09:38:59 +0530

    Update and rename pages/sitemap.xml.js to pages/api/sitemap.js

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,12 +1,7 @@
-export async function getServerSideProps({ res }) {
+export default function handler(req, res) {
   const baseUrl = "https://arttifai.tech";
 
-  const urls = [
-    "", // Homepage
-    "about",
-    "contact",
-    "internships",
-  ];
+  const urls = ["", "about", "contact", "internships"];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -24,13 +19,6 @@ export async function getServerSideProps({ res }) {
       .join("")}
   </urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
-  res.write(sitemap);
-  res.end();
-
-  return {
-    props: {},
-  };
+  res.setHeader("Content-Type", "application/xml");
+  res.status(200).send(sitemap);
 }
-
-export default function Sitemap() {}
